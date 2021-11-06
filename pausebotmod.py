9b751fd4d42bd24518c18b99d30bcba4de20edfc
Riccardo Esposito <espositoriccardo101@gmail.com>
2021-11-06 16:40:04 +0100

    enhancing pausebotmod

--- pausebotmod.py
+++ pausebotmod.py
@@ -7,41 +7,43 @@ INTERVAL = Interval.INTERVAL_1_MINUTE #Timeframe for analysis
 
 EXCHANGE = 'BINANCE'
 SCREENER = 'CRYPTO'
-
-# I don't agree that we should be looking at Bitcoin to figure out the pausing.
-SYMBOL = 'BTCUSDT'
-THRESHOLD = 16 # 10 of 15 MA's indicating sell
+SYMBOLS = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT']
+THRESHOLD = 13 # 7 of 15 MA's indicating sell
 TIME_TO_WAIT = 2 # Minutes to wait between analysis
 FULL_LOG = False # List analysis result to console
 
 def analyze():
-    analysis = {}
-    handler = {}
+
+    paused = True
+
+    for symbol in SYMBOLS:  
+        analysis = {}
+        handler = {}
+        
+        handler = TA_Handler(
+                symbol=symbol,
+                exchange=EXCHANGE,
+                screener=SCREENER,
+                interval=INTERVAL,
+                timeout= 10)
     
-    handler = TA_Handler(
-            symbol=SYMBOL,
-            exchange=EXCHANGE,
-            screener=SCREENER,
-            interval=INTERVAL,
-            timeout= 10)
- 
-    try:
-        analysis = handler.get_analysis()
-    except Exception as e:
-        print("pausebotmod:")
-        print("Exception:")
-        print(e)
-    
-    ma_sell = analysis.moving_averages['SELL']
-    print(analysis.moving_averages)
-    if ma_sell >= THRESHOLD:
-        paused = True
-        print(f'pausebotmod: Market not looking too good, bot paused from buying {ma_sell}/{THRESHOLD} Waiting {TIME_TO_WAIT} minutes for next market checkup')
-    else:
-        print(f'pausebotmod: Market looks ok, bot is running {ma_sell}/{THRESHOLD} Waiting {TIME_TO_WAIT} minutes for next market checkup ')
-        paused = False
+        try:
+            analysis = handler.get_analysis()
+        except Exception as e:
+            print("pausebotmod:")
+            print("Exception:")
+            print(e)
+        
+        ma_sell = analysis.moving_averages['SELL']
+        if ma_sell >= THRESHOLD:
+            paused = paused and True
+            print(f'pausebotmod: {symbol} Market not looking too good, bot paused from buying {ma_sell}/{THRESHOLD}.')
+        else:
+            print(f'pausebotmod: {symbol} Market looks ok, bot is running {ma_sell}/{THRESHOLD} Waiting {TIME_TO_WAIT} minutes for next market checkup ')
+            paused = False
 
     return paused
+    
 #if __name__ == '__main__':
 def do_work():
       
